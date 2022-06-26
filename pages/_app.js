9b4c8fcfb72db9a45ffc92f37cff96931e3e938f
Ved Gupta <vedgupta@protonmail.com>
2022-06-27 02:58:19 +0530

    Google Analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,27 @@
-import '../styles/globals.css';
-import States from '../context/data';
+import "../styles/globals.css";
+import States from "../context/data";
+import Script from "next/script";
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
 function MyApp({ Component, pageProps }) {
   return (
     <States>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </States>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
